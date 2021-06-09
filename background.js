adb39ecc958ed1b8e1b6b6751fe155f7a631569f
Niki Esfandiari <nikiesfandiari@nikis-air.attlocal.net>
2021-06-08 21:27:34 -0700

    realtime db start

--- background.js
+++ background.js
@@ -1,19 +1,58 @@
-var firebaseConfig = {
+self.importScripts(
+  './firebase/firebase-app.js',
+  './firebase/auth.js',
+  './firebase/database.js'
+);
+
+const firebaseConfig = {
   apiKey: 'AIzaSyDMsuM8d04SYP3ZNz6y0a8aqub2ihvOwvA',
   authDomain: 'schellit.firebaseapp.com',
   projectId: 'schellit',
   storageBucket: 'schellit.appspot.com',
   messagingSenderId: '241007919342',
+  databaseURL: 'https://schellit-default-rtdb.firebaseio.com',
   appId: '1:241007919342:web:33b3d0a77a7552d9f1b3ff',
   measurementId: 'G-B3S8BHVZPD',
+  clientId:
+    '630857334346-h666h86ugqa5p25mogd30jl51n2ek371.apps.googleusercontent.com',
 };
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log('From the Service Worker');
 });
 
-// chrome.browserAction.onClicked.addListener(function () {
-//   chrome.tabs.create({ url: 'index.html' });
-// });
+chrome.identity.onSignInChanged.addListener((thing) => {
+  console.log(thing);
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  const { message } = request;
+  if (message === 'store user') {
+    const { user } = request;
+    db.ref()
+      .child('users')
+      .child(user.id)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val(), 'val exists');
+        } else {
+          db.ref(`users/${user.id}`).set({
+            email: user.email,
+          });
+        }
+      });
+  }
+  if (message === 'store url') {
+    console.log(request);
+    const { user, urls } = request;
+    db.ref(`users/${user.id}`).set({
+      email: user.email,
+      urls: urls,
+    });
+  }
+  sendResponse({ greeting: 'farewell' });
+});
--- popup.js
+++ popup.js
@@ -1,15 +1,24 @@
 const submit = document.querySelector('#submit');
 const urlBar = document.querySelector('#urlBar');
 const urlList = document.querySelector('#urlList');
+const urlListContainer = document.querySelector('.urlList-container');
+const signUp = document.querySelector('#signup');
+const logout = document.querySelector('#logout');
 
 setUrlBar();
-
-chrome.storage.sync.get('urls', ({ urls }) => {
-  if (urls) {
-    for (let url of urls) {
-      url = shrinkUrl(url);
-      createUrl(url);
-    }
+chrome.identity.getProfileUserInfo((user) => {
+  if (user.email) {
+    signUp.classList.toggle('hidden');
+    logout.classList.toggle('hidden');
+    urlListContainer.classList.toggle('hidden');
+    chrome.storage.sync.get('urls', ({ urls }) => {
+      if (urls) {
+        for (let url of urls) {
+          url = shrinkUrl(url);
+          createUrl(url);
+        }
+      }
+    });
   }
 });
 
@@ -17,19 +26,55 @@ submit.addEventListener('click', async () => {
   const tab = await getCurrentTab();
   const cleanUrl = shrinkUrl(tab.url);
   chrome.storage.sync.get('urls', ({ urls }) => {
-    console.log(urls, 'urls');
     if (urls && urls.includes(cleanUrl)) {
       alert('url already is being listened on');
-      return;
-    }
-    if (!urls || urls.length < 5) {
+    } else if (!urls || urls.length < 5) {
       const newUrls = urls ? [...urls, cleanUrl] : [cleanUrl];
+
       chrome.storage.sync.set({ urls: newUrls });
+      chrome.identity.getProfileUserInfo((user) => {
+        chrome.runtime.sendMessage({
+          user,
+          urls: newUrls,
+          message: 'store url',
+        });
+      });
+
+      createUrl(cleanUrl);
     } else {
       alert('already listening on 5 urls');
     }
   });
-  createUrl(cleanUrl);
+});
+
+signUp.addEventListener('click', async () => {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    console.log(token);
+    var x = new XMLHttpRequest();
+    x.open(
+      'GET',
+      'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' +
+        token
+    );
+    x.onload = function () {
+      console.log(x.response);
+      chrome.identity.getProfileUserInfo((user) => {
+        console.log(user);
+        chrome.runtime.sendMessage({ user, message: 'store user' });
+      });
+    };
+    x.send();
+  });
+});
+
+logout.addEventListener('click', () => {
+  chrome.identity.getAuthToken({ interactive: false }, function (token) {
+    var url = 'https://accounts.google.com/o/oauth2/revoke?token=' + token;
+    window.fetch(url);
+    chrome.identity.removeCachedAuthToken({ token: token }, function () {
+      alert('removed');
+    });
+  });
 });
 
 function createUrl(url) {
